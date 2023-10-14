ac333ef47d0c9338aaab2d41508b45e169f10530
Lise-Brisset <lise.brisset9@gmail.com>
2023-10-14 15:41:46 +0200

    Correction erreur question2 exo1 arguments

--- Exercices/seance4/script_exo12.sh
+++ Exercices/seance4/script_exo12.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/bash
-echo "Nombre de $0 en 2016 : "
-cat ~/TAL/github/PPE1-2023/Fichiers/ann/2016/*/* | grep $0 | wc -l
-echo "Nombre de $0 en 2017 : "
-cat ~/TAL/github/PPE1-2023/Fichiers/ann/2017/*/* | grep $0 | wc -l
-echo "Nombre de $0 en 2018 : "
-cat ~/TAL/github/PPE1-2023/Fichiers/ann/2018/*/* | grep $0 | wc -l
+echo "Nombre de $1 en 2016 : "
+cat ~/TAL/github/PPE1-2023/Fichiers/ann/2016/*/* | grep $1 | wc -l
+echo "Nombre de $1 en 2017 : "
+cat ~/TAL/github/PPE1-2023/Fichiers/ann/2017/*/* | grep $1 | wc -l
+echo "Nombre de $1 en 2018 : "
+cat ~/TAL/github/PPE1-2023/Fichiers/ann/2018/*/* | grep $1 | wc -l
 
