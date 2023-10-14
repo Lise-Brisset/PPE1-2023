928f415b873eee22cbb5c89dad7d8a1ff4017d58
Lise-Brisset <lise.brisset9@gmail.com>
2023-10-14 13:55:41 +0200

    Ajout du script.sh

---
+++ Exercices/seance4/script.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+#nombre de Location par année : 
+cat ~/TAL/github/PPE1-2023/Fichiers/ann/2016/*/* | grep Location | wc -l
+cat ~/TAL/github/PPE1-2023/Fichiers/ann/2017/*/* | grep Location | wc -l
+cat ~/TAL/github/PPE1-2023/Fichiers/ann/2018/*/* | grep Location | wc -l
