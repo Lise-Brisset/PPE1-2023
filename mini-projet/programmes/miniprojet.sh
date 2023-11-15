4fdaf2a17cea0ece65f7a969e01685c398a1147a
Lise-Brisset <lise.brisset9@gmail.com>
2023-11-15 12:57:54 +0100

    correction miniprojet.sh

--- mini-projet/programmes/miniprojet.sh
+++ mini-projet/programmes/miniprojet.sh
@@ -1,47 +1,50 @@
 #!/usr/bin/env bash
 
 #lignes permettant la vérifications des arguments : 
-if [ $# -ne 2 ]
+if [ $# -ne 1 ]
 then
-	echo "deux argument attendu exactement"
+	echo "un argument attendu exactement"
 	exit
 else
-	if [ -f $1 ]
+
+if [ ! -f ../urls/$1 ]
 	then
-		if [ -f $2 ]
-		then
-			echo "on bien deux fichiers existants"
-		else
-			echo "le fichier n'existe pas"
-			exit
-		fi
+		echo "le fichier n'existe pas"
+		exit
 	fi
 fi
 
-#construction des variables utile dans la construction de notre tableau, les arguments sont le fichier texte d'urls et le fichier html du tableau : 
+#construction des variables utile dans la construction de notre tableau, le premier argument est le fichier texte d'urls:
 URLS=$1
 lineno=1
-tableau=$2
 
 #construction du début du fichier html avec les métadonnées : 
 echo "<html>
 	<head>
-		<head charset="UTF-8"/>
+		<meta charset="UTF-8"/>
 	</head>
-	<body>
-		<table>" > ../tableaux/$tableau
+	<body>"
+
+
+echo "		<table>
+			<tr><th>ligne</th><th>URL</th><th>code HTTP</th><th>encodage</th></tr> "
 
 #remplissage du tableau html : 
 while read -r URL
 do
 	reponse=$(curl -s -I -L -w "%{http_code}" -o /dev/null $URL)
 	encodage=$(curl -s -I -L -w "%{content_type}" -o /dev/null $URL | grep -P -o "charset=\S+" | cut -d"=" -f2 | tail -n 1)
-	echo "				<tr><th>$lineno</th><td>$URL</td><td>$reponse</td><td>$encodage</td></tr>" >> ../tableaux/$tableau
+	echo "				<tr>
+					<td>$lineno</td>
+					<td>$URL</td>
+					<td>$reponse</td>
+					<td>$encodage</td>
+				</tr>"
 	lineno=$(expr $lineno + 1)
 done < ../urls/$URLS
 
 #fermetures du tableau avec les balises fermantes html : 
 echo " 		</table>
 	</body>
-</html>" >> ../tableaux/$tableau
+</html>"
 
