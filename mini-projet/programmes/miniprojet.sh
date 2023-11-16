92bf0d343a6f38851e4fb4715e980c19641214ae
Lise-Brisset <lise.brisset9@gmail.com>
2023-11-16 16:40:18 +0100

    mise à jour script avec bulma

--- mini-projet/programmes/miniprojet.sh
+++ mini-projet/programmes/miniprojet.sh
@@ -21,30 +21,33 @@ lineno=1
 #construction du début du fichier html avec les métadonnées : 
 echo "<html>
 	<head>
-		<meta charset="UTF-8"/>
+		<meta charset=\"UTF-8\"/>
+		<link rel=\"stylesheet\" href=\"https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css\">
 	</head>
-	<body>"
+	<body class=\"has-background-grey-lighter\">"
 
 
-echo "		<table>
-			<tr><th>ligne</th><th>URL</th><th>code HTTP</th><th>encodage</th></tr> "
+echo "		<div class=\"hero-body\">
+			<table class=\"table is-striped is-fullwidth\">
+				<tr class=\"title is-5\"><th>ligne</th><th>URL</th><th>code HTTP</th><th>encodage</th></tr> "
 
 #remplissage du tableau html : 
 while read -r URL
 do
 	reponse=$(curl -s -I -L -w "%{http_code}" -o /dev/null $URL)
 	encodage=$(curl -s -I -L -w "%{content_type}" -o /dev/null $URL | grep -P -o "charset=\S+" | cut -d"=" -f2 | tail -n 1)
-	echo "				<tr>
-					<td>$lineno</td>
-					<td>$URL</td>
-					<td>$reponse</td>
-					<td>$encodage</td>
-				</tr>"
+	echo "					<tr>
+						<td>$lineno</td>
+						<td>$URL</td>
+						<td>$reponse</td>
+						<td>$encodage</td>
+					</tr>"
 	lineno=$(expr $lineno + 1)
 done < ../urls/$URLS
 
 #fermetures du tableau avec les balises fermantes html : 
-echo " 		</table>
+echo " 			</table>
+		</div>
 	</body>
 </html>"
 
